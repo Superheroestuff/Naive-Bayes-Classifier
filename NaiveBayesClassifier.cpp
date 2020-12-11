c22ab07c63b835db8a60b1d29ca089ebbd7aa60b
Superheroestuff <37773448+Superheroestuff@users.noreply.github.com>
2020-12-11 18:46:14 +0200

    struct Person, readFile

--- NaiveBayesClassifier.cpp
+++ NaiveBayesClassifier.cpp
@@ -1,18 +1,34 @@
 #include<iostrema>
+#include<fstream>
 using namespace std;
 
-Person
+struct Person
 {
     char attributes[16];
     char party[20];
 }
 
 Person people[500];
+int peopleCnt;
 
-
-void readFile()
+void readFile(const char* fileName)
 {
+    ifstream file;
+    file.open("fileName");
+    if(!file.is_open())
+    {
+        cout<<"File did not opened!"<<endl;
+        return;
+    }
 
+    peopleCnt = 0;
+
+    char c;
+    while(file.get(c))
+    {
+
+
+    }
 
 }
 
