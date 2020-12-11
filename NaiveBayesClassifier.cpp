261f6b3fabc66b5c6739d6f5d9c6d833033c19de
Superheroestuff <37773448+Superheroestuff@users.noreply.github.com>
2020-12-11 18:33:01 +0200

    added data file

--- NaiveBayesClassifier.cpp
+++ NaiveBayesClassifier.cpp
@@ -1,1 +1,24 @@
+#include<iostrema>
+using namespace std;
 
+Person
+{
+    char attributes[16];
+    char party[20];
+}
+
+Person people[500];
+
+
+void readFile()
+{
+
+
+}
+
+
+int main()
+{
+
+    return 0;
+}
