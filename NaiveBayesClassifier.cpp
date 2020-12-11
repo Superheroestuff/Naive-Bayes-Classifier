bc0f2692b433a384b05234c6b49ae6044d1e05c5
Superheroestuff <37773448+Superheroestuff@users.noreply.github.com>
2020-12-11 19:04:02 +0200

    read data from file and store it in a structure

--- NaiveBayesClassifier.cpp
+++ NaiveBayesClassifier.cpp
@@ -2,14 +2,14 @@
 #include<fstream>
 using namespace std;
 
-struct Person
+struct Person//tuple from the data set
 {
     char attributes[16];
     char party[20];
 }
 
-Person people[500];
-int peopleCnt;
+Person people[500];//all people from the data set
+int peopleCnt;//number of people in the data set
 
 void readFile(const char* fileName)
 {
@@ -22,14 +22,38 @@ void readFile(const char* fileName)
     }
 
     peopleCnt = 0;
+    int attr = 0;
 
     char c;
     while(file.get(c))
     {
-
+        if(c == '\n') //we've done reading the current person, start reading the next
+        {
+            peopleCnt++;//add new person
+            attr = 0;//start attributes from beginning
+        }
+        else if((c >= 'a' && c <= 'z') || c == '?')
+        {
+            if(c == 'y'||c == 'n'||c == '?')
+            {
+                people[peopleCnt].attributes[attr] = c;
+                attr++;//going to next attribute
+            }
+            else//read the party name (class name)
+            {
+                int i=0;
+                while(c != ',')
+                {
+                    people[peopleCnt].party[i] = c;
+                    i++;
+                    file.get(c);
+                }
+                people[peopleCnt].party[i] = '\0';//put an end to the party name
+            }
+        }
 
     }
-
+    peopleCnt++;//the last person from the data set
 }
 
 
